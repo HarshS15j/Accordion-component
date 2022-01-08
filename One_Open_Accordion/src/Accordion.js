3c2dcc151c165571c553ded13b1f634f8e3f8c83
harsh1507 <harshsingh15j@gmail.com>
2022-01-08 17:43:35 +0530

    Only_OneOpen_Accordion_completed

--- One_Open_Accordion/src/Accordion.js
+++ One_Open_Accordion/src/Accordion.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 
-const Accordion = ({ title, content }) => {
+const Accordion = ({ title, content, index }) => {
   const [isActive, setIsActive] = useState(false);
 
+  //const [allinactive, setAllinactive] = useState(false);
+
+  const changetoActive = ()=>{
+
+    setIsActive(!isActive);
+    //setAllinactive(false);
+
+  }
+
   return (
-    <div className="accordion-item">
-      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+    <div id = {index} className="accordion-item">
+      <div className="accordion-title" onClick={() => changetoActive()}>
         <div>{title}</div>
-        <div>{isActive ? '-' : '+'}</div>
+        <div>{(isActive) ? '-' : '+'}</div>
       </div>
-      {isActive && <div className="accordion-content">{content}</div>}
+      {(isActive) && <div className="accordion-content">{content}</div>}
     </div>
   );
 };
--- One_Open_Accordion/src/App.js
+++ One_Open_Accordion/src/App.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import Accordion from './Accordion';
-import { accordionData } from './utils/data';
+import React, { useEffect } from "react";
+//import Accordion from "./Accordion";
+import { accordionData } from "./utils/data";
+import { useState } from "react";
 
 const App = () => {
+  const [isActive, setIsActive] = useState([]);
+
+  //const [allinactive, setAllinactive] = useState(false);
+
+  useEffect(()=>{
+    const defaultStates = [];
+    for(let i=0; i<accordionData.length;i++){
+      defaultStates.push(false);
+    };
+    
+    setIsActive(defaultStates);
+  },[]);
+
+
+  const changetoActive = (index) => {
+    const newStates =[];
+
+    for(let i=0; i<accordionData.length;i++){
+      if(i===parseInt(index)){
+        newStates.push(true);  
+      }else{
+      newStates.push(false);
+    }
+    };
+    setIsActive(newStates);
+
+    //setAllinactive(false);
+  };
 
   return (
     <div>
       <h1>React Accordion Demo</h1>
       <div className="accordion">
-        {accordionData.map(({ title, content }) => (
-          <Accordion title={title} content={content} />
+        {accordionData.map((item, index) => (
+          <div id={index} className="accordion-item">
+            <div className="accordion-title" onClick={() => changetoActive(index)}>
+              <div>{item.title}</div>
+              <div>{isActive[index] ? "-" : "+"}</div>
+            </div>
+            {isActive[index] && (
+              <div className="accordion-content">{item.content}</div>
+            )}
+          </div>
+
+          //<Accordion title={item.title} content={item.content} index={index} />
         ))}
       </div>
     </div>
