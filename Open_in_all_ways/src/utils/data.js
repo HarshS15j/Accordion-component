ebf97cd0212347323cb9b1bb66b4662dbea082a2
harsh1507 <harshsingh15j@gmail.com>
2022-01-12 01:55:55 +0530

    accordion_component_in_all_possible_ways_completed

---
+++ Open_in_all_ways/src/utils/data.js
@@ -0,0 +1,44 @@
+export const accordionData = [
+  {
+    title: "Main features of OOPs",
+    content: `OOPs or Object Oriented Programming mainly comprises of the below four features,
+      and make sure you don't miss any of these:
+      Inheritance,
+      Encapsulation,
+      Polymorphism,
+      Data Abstraction.`,
+  },
+  {
+    title: "Why is OOPs so popular?",
+    content: `OOPs programming paradigm is considered as a better style of programming. Not only it
+    helps in writing a complex piece of code easily, but it also allows users to handle and maintain
+    them easily as well.`,
+  },
+  {
+    title: "Define Data Hiding and Data Binding.",
+    content: `Data hiding in terms of encapsulation is the process of hiding unwanted information, 
+    such as restricting access to any member of an object. Where as Data binding in terms of encapsulation
+    is the process of binding the data members and the methods together as a whole, as a class.`,
+  },
+  {
+    title: "Main features of OOPs",
+    content: `OOPs or Object Oriented Programming mainly comprises of the below four features,
+      and make sure you don't miss any of these:
+      Inheritance,
+      Encapsulation,
+      Polymorphism,
+      Data Abstraction.`,
+  },
+  {
+    title: "Why is OOPs so popular?",
+    content: `OOPs programming paradigm is considered as a better style of programming. Not only it
+    helps in writing a complex piece of code easily, but it also allows users to handle and maintain
+    them easily as well.`,
+  },
+  {
+    title: "Define Data Hiding and Data Binding.",
+    content: `Data hiding in terms of encapsulation is the process of hiding unwanted information, 
+    such as restricting access to any member of an object. Where as Data binding in terms of encapsulation
+    is the process of binding the data members and the methods together as a whole, as a class.`,
+  },
+];
